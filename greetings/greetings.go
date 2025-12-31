97f485f8a2665e8df649fbfa1ac1a7cc9c2c01be
Bello Olamilekan <belloolamilekan661@gmail.com>
2025-12-31 11:07:54 +0100

    feat: add error handling and random greeting formats to greetings module
    Update Hello function to return error for empty name parameter and select random greeting format from predefined templates. Add randomFormat helper function that returns one of three greeting templates using math/rand. Update hello package to handle error return value with log.Fatal for error cases.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Name is required")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail %v, well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
